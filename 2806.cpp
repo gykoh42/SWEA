498f2298d041633b0e927bd51d28a134be948a55
gykoh42 <karmakoh42@naver.com>
2024-06-05 23:58:00 +0900

    feat: 단계별로 풀어보기 진행중

--- 2806.cpp
+++ 2806.cpp
@@ -1,40 +1,38 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int arr[50][50];
+int N, ans;
+int col[11];
+
+bool check(int row) {
+	for (int i = 0; i < row; i++) {
+		if (col[i] == col[row] || abs(col[row] - col[i]) == row - i) {
+			return false;
+		}
+	}
+	return true;
+}
+
+void nQueen(int row) {
+	if (row == N) {
+		ans++;
+	}
+	for (int i = 0; i < N; i++) {
+		col[row] = i;
+		if (check(row)) {
+			nQueen(row + 1);
+		}
+	}
+}
 
 int main() {
 	int T;
 	cin >> T;
 	for (int tc = 1; tc <= T; tc++) {
-		int N;
 		cin >> N;
-		for (int i = 0; i < N; i++) {
-			string line;
-			cin >> line;
-			for (int j = 0; j < N; j++) {
-				arr[i][j] = line[j] - '0';
-			}
-		}
-		int ans = 0;
-		int idx_flag = N / 2;
-		int cnt = 1;
-		for (int i = 0; i <= N / 2; i++) {
-			for (int j = idx_flag; j < idx_flag + cnt; j++) {
-				ans += arr[i][j];
-			}
-			idx_flag--;
-			cnt += 2;
-		}
-		idx_flag = 1;
-		cnt -= 4;
-		for (int i = N / 2 + 1; i < N; i++) {
-			for (int j = idx_flag; j < idx_flag + cnt; j++) {
-				ans += arr[i][j];
-			}
-			idx_flag++;
-			cnt -= 2;
-		}
+		memset(col, 0, sizeof(col));
+		nQueen(0);
 		cout << "#" << tc << " " << ans << "\n";
 	}
 }
